ac31393d002f2bd2b9d44af75aa611cf94dbfd56
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 19:39:01 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+# doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+puts doc.css(".title-oE5vT4").text
