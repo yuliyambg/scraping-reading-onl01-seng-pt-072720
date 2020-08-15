af629a1db174679196b3a9f0dbbee1b6263c8ef8
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-15 19:35:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ html = open("https://flatironschool.com/")
 # doc = Nokogiri::HTML(html)
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-26OIBN").text
+puts doc.css(".headline-26OIBN").text
